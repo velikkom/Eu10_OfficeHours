ff909dc9a66744711fd2dc814141a48226e50bd8
Veli Kara <velikkom@hotmail.com>
2022-12-07 21:41:00 +0300

    addet extra info

--- src/week7/CustomMethod.java
+++ src/week7/CustomMethod.java
@@ -3,7 +3,11 @@ package week7;
 public class CustomMethod {
     public static void main(String[] args) {
 
+manualTesting("veli");
+    }
 
+    private static void manualTesting(String name) {
+        System.out.println(name+" is testing manuly");
     }
 
 }
